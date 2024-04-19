fda4741270778943edac873c04d287c873438eda
NikhilMathew31 <mathewnikhil432@gmail.com>
2024-04-19 20:55:24 +0530

    initial 03

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ class Circle {
     double area,radius;
     public void get() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the radius: ");
+        System.out.print("Enter the radius: ");
         radius = sc.nextDouble();
         area = 3.14 * radius * radius;
         System.out.println("Area of circle: " + area);
@@ -21,6 +21,15 @@ public class Main {
         System.out.println("Length of array: " + arr.length);
         for(int i=0; i<arr.length; i++) {
             System.out.println(arr[i]);
+        }
+
+        int[][] a = {{1,2},{3,4}};
+        System.out.println("TwoD array: ");
+        for(int i=0; i<2; i++) {
+            for(int j=0; j<2; j++) {
+                System.out.print(a[i][j] + "\t");
+            }
+            System.out.println();
         }
 
         Circle c = new Circle();
