2075f98a7556d48dd28395bfc1b9c4f7c20be2f4
NikhilMathew31 <mathewnikhil432@gmail.com>
2024-04-19 20:43:20 +0530

    initial 02

--- src/Main.java
+++ src/Main.java
@@ -11,12 +11,20 @@ class Circle {
         System.out.println("Area of circle: " + area);
     }
 }
+
 public class Main {
     public static void main(String[] args) {
         Date now = new Date();
         System.out.println("Today: " + now);
 
+        int[] arr = { 1, 2, 3, 4, 5 };
+        System.out.println("Length of array: " + arr.length);
+        for(int i=0; i<arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+
         Circle c = new Circle();
         c.get();
+
     }
 }
