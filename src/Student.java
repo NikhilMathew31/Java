b61b2baf001f23afc9e0a6301961b98780acaa10
NikhilMathew31 <mathewnikhil432@gmail.com>
2024-04-22 19:49:53 +0530

    update 06

---
+++ src/Student.java
@@ -0,0 +1,34 @@
+public class Student {
+    String name;
+    int age;
+    String address;
+
+    public Student(String name, int age, String address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Address: " + address);
+    }
+
+    public static void main(String[] args) {
+        Student stud = new Student("Nikhil", 22, "Trivandrum");
+        stud.display();
+    }
+}
